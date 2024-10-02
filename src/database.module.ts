72213d8dfedac688868d11cfcb0b000548f39705
Dominik Bošnjak <dominik.bosnjak94@gmail.com>
2024-10-02 20:03:06 +0200

    adding database.model.ts

---
+++ src/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './users/users.entity';
+import { Task } from './tasks/task.entity';
+import { Category } from './categories/category.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User, Task, Category],
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
