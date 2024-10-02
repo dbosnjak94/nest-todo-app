b19f7220b064b14075e603fee4160884af643b2e
Dominik Bošnjak <dominik.bosnjak94@gmail.com>
2024-10-02 20:03:43 +0200

    Updating app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { CategoriesModule } from './categories/categories.module';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UsersModule,
+    TasksModule,
+    AuthModule,
+    CategoriesModule,
+    AppModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
