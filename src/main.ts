0a83c484a70645c9398f1f211b47067da29622b5
Dominik Bošnjak <dominik.bosnjak94@gmail.com>
2024-10-02 23:47:17 +0200

    Adding Swagger to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global guards
+  // app.useGlobalGuards(new JwtAuthGuard());
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Urbify Todo App Interview Task')
+    .setDescription('The Urbify Todo API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
