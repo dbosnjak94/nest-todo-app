75e94f89fea30739190a8d9686ea9fc361a402b4
Dominik Bošnjak <dominik.bosnjak94@gmail.com>
2024-10-03 10:07:59 +0200

    Updatign user.controller with swagger documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,13 +4,19 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @ApiTags('users')
@@ -20,6 +26,10 @@ export class UsersController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'The User has been successfully created',
+  })
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
@@ -28,6 +38,7 @@ export class UsersController {
   // @UseGuards(JwtAuthGuard)
   // @ApiBearerAuth()
   @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Return all users' })
   getAllUsers() {
     return this.userService.findAllUsers();
   }
@@ -35,27 +46,39 @@ export class UsersController {
   @Get(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'Return the user' })
+  @ApiResponse({ status: 404, description: 'User not found' })
   @ApiOperation({ summary: 'Get user by ID' })
   getUserById(@Param('id') userId: string) {
     return this.userService.findUserById(userId);
   }
 
-  @Post(':id')
+  @Patch(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @ApiOperation({ summary: 'Update user' })
+  @ApiOperation({ summary: 'Update a user' })
+  @ApiResponse({
+    status: 200,
+    description: 'The user has been successfully updated',
+  })
+  @ApiResponse({ status: 404, description: 'User not found' })
   updateUser(
     @Param('id') userId: string,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.userService.update(userId, updateUserDto);
+    return this.userService.updateUser(userId, updateUserDto);
   }
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Delete user' })
+  @ApiResponse({
+    status: 200,
+    description: 'The user has been suffessfully deleted',
+  })
+  @ApiResponse({ status: 404, description: 'User not found' })
   deleteUser(@Param('id') userId: string) {
-    return this.userService.remove(userId);
+    return this.userService.deleteUser(userId);
   }
 }
